801fd07dc01cf1f72cfac3b898a9519ce1fcb748
sabinnepal599 <163380551+sabinnepal599@users.noreply.github.com>
2024-05-06 12:12:08 +0545

    Add files via upload

---
+++ data/Bhotekoshi_Evacuation_Center.js
@@ -0,0 +1,8 @@
+var Bhotekoshi_Evacuation_Center = {
+"type": "FeatureCollection",
+"name": "Bhotekoshi_Evacuation_Center",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ward": 0, "Tole": null, "latitude": 27.960504973300001, "longitude": 85.952274118600002, "Institutio": "Evacuation Center", "Name": "Bhotekoshi Emergency Shelter", "Insti_nep": "cfkt\\sfnLg cf>o :yn", "Nep_Name": "ef]6]sf]zL cfkt\\sfnLg cf>o :yn", "Capacity": "50", "Room": 2, "PWD_inclus": "Yes" }, "geometry": { "type": "Point", "coordinates": [ 85.952281732904126, 27.960598248145462 ] } }
+]
+}
---
+++ data/Bhotekoshi_cat_III.js
@@ -0,0 +1,11 @@
+var Bhotekoshi_cat_III = {
+"type": "FeatureCollection",
+"name": "Bhotekoshi cat III",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Milan Basti", "Category": "III", "HHs": "19", "Latitide": 27.961484888600001, "Longitude": 85.955987510300005, "Nep_name": "ldng a:tL", "Ward": 2, "Cat_nep": "#", "Ward_Nep": "@", "CatI_HHs": "0", "CatII_HHs": 0, "CatIII_HHs": "19", "P_D__HHs": "1" }, "geometry": { "type": "Point", "coordinates": [ 85.955987510296623, 27.961484888606453 ] } },
+{ "type": "Feature", "properties": { "Name": "Sangapani", "Category": "III", "HHs": "1", "Latitide": 27.867104049600002, "Longitude": 85.941684733800003, "Nep_name": ";fªkfgL", "Ward": 5, "Cat_nep": "#", "Ward_Nep": "%", "CatI_HHs": "0", "CatII_HHs": 0, "CatIII_HHs": "1", "P_D__HHs": null }, "geometry": { "type": "Point", "coordinates": [ 85.941684733777919, 27.867104049550605 ] } },
+{ "type": "Feature", "properties": { "Name": "Dhanapal", "Category": "III", "HHs": "7", "Latitide": 27.887635466799999, "Longitude": 85.874105905999997, "Nep_name": "wgkfn", "Ward": 1, "Cat_nep": "#", "Ward_Nep": "!", "CatI_HHs": "0", "CatII_HHs": 0, "CatIII_HHs": "7", "P_D__HHs": null }, "geometry": { "type": "Point", "coordinates": [ 85.874105906005497, 27.887635466785721 ] } },
+{ "type": "Feature", "properties": { "Name": "Larcha", "Category": "III", "HHs": "20", "Latitide": 27.932115570099999, "Longitude": 85.936443170199993, "Nep_name": "nfrf{", "Ward": 3, "Cat_nep": "#", "Ward_Nep": "#", "CatI_HHs": "0", "CatII_HHs": 0, "CatIII_HHs": "20", "P_D__HHs": "2" }, "geometry": { "type": "Point", "coordinates": [ 85.936443170187943, 27.932115570128065 ] } }
+]
+}
